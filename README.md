586b057f7afc513aa07f82661d7f31115c032eff
Pieter Vleminckx <pieter.vlem@gmail.com>
2018-08-13 22:13:57 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CMSDEV_Herex
+
+- Drupal hosted with mamp
+- `Yarn start` to start Vue app
---
+++ htdocs - Drupal/themes/custom/beelectric/scripts/app.js
@@ -0,0 +1,3 @@
+(function($){
+    $( ".path-frontpage .views-row" ).wrap( "<div class='view-row-wrapper'></div>" );
+})(jQuery)
